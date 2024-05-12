280f55376f137c38162658a49f8eac5a301a124b
Sanket Bhadange <sanketnb2404@gmail.com>
2024-05-12 23:21:36 +0530

    redux

---
+++ src/components/Products.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { add } from "../store/cartSlice";
+
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setProducts(response.data);
+    };
+
+    getProducts();
+  }, []);
+
+  const dispatcher = useDispatch()
+
+  const addToCart = (product) => {
+    
+    dispatcher(add(product))
+  };
+
+  return (
+    <div className="productsWrapper">
+      
+      {products.map((product) => {
+        return (
+          <div className="card">
+            <img src={product.image} alt="product" />
+            <h6>{product.title}</h6>
+            <h5>{product.price}</h5>
+            <button className="btn" onClick={() => addToCart(product)}>
+              Add to Cart
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Products;
