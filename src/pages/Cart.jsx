f05b4afcd7763c496303b61bf864c60e107afe39
Sanket Bhadange <sanketnb2404@gmail.com>
2024-05-15 22:42:16 +0530

    Thunk Function Created

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
+  const items= useSelector(state =>state.cart)
   return (
-    <div>Cart</div>
+    <div className='cartWrapper'>
+    {
+      items.map(item => {
+        return <div className='cartCard'>
+          <img src={item.image} alt='product'/>
+          <h5>{item.title}</h5>
+          <h5>Price: ${item.price}</h5>
+
+          
+        </div>
+      })
+    }
+  </div>
   )
 }
 
